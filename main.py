ac8a27b1e88083a4aa7a3c398c0105366f562b4a
Kmiles <71646271+Kdm2633@users.noreply.github.com>
2022-05-21 21:30:29 +0000

    markers arent working

--- main.py
+++ main.py
@@ -24,10 +24,7 @@ def homepage():
 # Refugee Data
 refugee_data = pd.read_csv('data/UkraineRefugees.csv')
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-refugee_data = world.merge(refugee_data,
-                           how='left',
-                           left_on=['name'],
-                           right_on=['Name'])
+refugee_data = world.merge(refugee_data, how='left', left_on=['name'], right_on=['Name'])
 cols = refugee_data.columns[6:]
 refugee_data = refugee_data.dropna(subset=cols, how='all')
 pd.set_option('display.max_columns', 20)
@@ -47,29 +44,28 @@ m = folium.Map(
     zoom_start=4,
     min_zoom=4,
     max_zoom=7,
-    tiles=
-    "https://api.mapbox.com/styles/v1/lumii/cl3g4e55100aq14qumgn4whkg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVtaWkiLCJhIjoiY2wzZzN3ZGEyMDVnZjNmcDc3Z3Jodm0wbyJ9.oTc222D0JZekjMQD_1EGYw",
+    tiles="https://api.mapbox.com/styles/v1/lumii/cl3g4e55100aq14qumgn4whkg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVtaWkiLCJhIjoiY2wzZzN3ZGEyMDVnZjNmcDc3Z3Jodm0wbyJ9.oTc222D0JZekjMQD_1EGYw",
     attr="mapbox")
 # folium.GeoJson(data=ukr_map["geometry"]).add_to(m)
 # Creating Markers
 print("worked 1")
-folium.CircleMarker(location=[50.000, 25.0000],
-              radius=50,color="#ffffff").add_to(m)
+# folium.CircleMarker(location=[50.000, 25.0000], radius=50,
+#                     color="#ffffff").add_to(m)
 print("worked 2")
-# folium.Choropleth(
-#     geo_data=ukr_geo,
-#     name="choropleth",
-#     data=healthcare_data,
-#     columns=["Region", "Health_Facilities"],
-#     key_on="feature.id",
-#     fill_color="YlGn",
-#     fill_opacity=0.7,
-#     line_opacity=.1,
-#     legend_name="Healthcare Facilities",
-# ).add_to(m)
+folium.Choropleth(
+    geo_data=ukr_geo,
+    name="choropleth",
+    data=healthcare_data,
+    columns=["Region", "Health_Facilities"],
+    key_on="feature.id",
+    fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=.1,
+    legend_name="Healthcare Facilities",
+).add_to(m)
 print("worked 3")
 
-#folium.LayerControl().add_to(m)
+folium.LayerControl().add_to(m)
 m.save('./templates/map.html')
 
 # Running comes last
